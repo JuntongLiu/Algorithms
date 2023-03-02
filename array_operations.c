cae558096b69c7e61010953345e5da68fc88a22f
JuntongLiu <49035076+JuntongLiu@users.noreply.github.com>
2023-03-02 19:00:34 +0100

    Tidy up print out

--- array_operations.c
+++ array_operations.c
@@ -87,33 +87,39 @@ static void find_even_elements(int *array, int len, int *evenelem, int *rlen)
 
 int main()
 {
-    int len = 10;
     int array[] = {0,1,2,3,4,5,6,7,8,9};
+    size_t len = sizeof(array) / sizeof(array[0]);
     int evenelem[len];
     int elen;
 
+    printf("\nOriginal array elements:\n");
     for(int i=0; i<len; i++)
-        printf("Original element %d is: %d\n", i, array[i]);
+        printf("%d  ", array[i]);
 
     // reverse the array
     reverse_int_array(array, len);
+    printf("\n\nArray after reversing: \n");
     for(int i=0; i<len; i++)
-        printf("Array after reversing, element %d is: %d\n", i, array[i]);
+        printf("%d  ", array[i]);
 
     // sort the array
     qsort(array, len, sizeof(int), mycompare);
+    printf("\n\nArray after sorted acendingly: \n");
     for(int i=0; i<len; i++)
-        printf("Array after sorted acendingly, element %d is: %d\n", i, array[i]);
+        printf("%d  ", array[i]);
 
     // reverse and move array begining section
     reverse_and_move_section(array, len, 3);
+    printf("\n\nArray has reversed-begining-section moved to tail: \n");
     for(int i=0; i<len; i++)
-        printf("Array after moved the reversed begining section to the tail, element %d is: %d\n", i, array[i]);
+        printf("%d  ", array[i]);
 
     // find even elements in an integer array
     find_even_elements(array, len, evenelem, &elen);
+    printf("\n\nArray has following even elements: \n");
     for(int i=0; i<elen; i++)
-        printf("Array has even element: %d \n", evenelem[i]);
-
+        printf("%d  ", evenelem[i]);
+    printf("\n\n");
+   
     return 0; 
 }
