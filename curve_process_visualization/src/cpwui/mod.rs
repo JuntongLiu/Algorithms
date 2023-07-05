b0336fd84fb5597af2b5cd81f0237675e3e19b9c
JuntongLiu <49035076+JuntongLiu@users.noreply.github.com>
2023-07-05 17:31:42 +0200

    Update mod.rs
    Add comments

--- curve_process_visualization/src/cpwui/mod.rs
+++ curve_process_visualization/src/cpwui/mod.rs
@@ -1,3 +1,13 @@
+/* 
+ * Juntong Liu
+ *         2023.Jun 28
+ * 
+ * File name:    curve_process_visualization/src/cpwui/mod.rs
+ * version 0.1
+ * 
+ * This program has been coded just for fine. It has not been tested.  Use it is at your own risk.
+ *
+ **/
 
 use std::fs::File;
 use std::io::{self, BufRead};
