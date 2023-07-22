3ab61c9503baaed64d838067932a909890cae5c5
JuntongLiu <49035076+JuntongLiu@users.noreply.github.com>
2023-07-22 18:39:24 +0200

    Update main.rs
    Add new UI widgets

--- curve_process_visualization/src/main.rs
+++ curve_process_visualization/src/main.rs
@@ -3,7 +3,7 @@
  *         2023.June.28
  * 
  * File name: curve_process_visualization/src/main.rs   
- * version 0.1
+ * version 0.11
  * 
  * This program has been coded just for fine. It has not been tested.  Use it is at your own risk.
  *
@@ -19,6 +19,7 @@ use cpwui::*;
 pub enum Message {
     Increment,
     Decrement,
+    Choice1Msg,
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -36,6 +37,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let (s, r) = app::channel::<Message>();
     ui.plus_but.emit(s, Message::Increment);
     ui.minus_but.emit(s, Message::Decrement);
+    ui.choice_1.emit(s, Message::Choice1Msg);
 
     let mut buf = vec![0u8; 670 * 480 * 3];
 
@@ -45,32 +47,57 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
           if let Some(msg) = r.recv() {
               match msg {
                     Message::Increment => {
-                        ui.frame_num_bps.set_label(&(label +1).to_string());
-                    }
+			if pcbp.process_bps(1) == Ok(0) {
+                        	ui.frame_num_bps.set_label(&(label +1).to_string());
+                    		if pcbp.get_driver_flag() == false {
+					pcbp.set_driver_flag();
+				}
+			}
+			else {
+				panic!("Add BP error! Error process curve breakpoints!");
+			}
+		    }
                     Message::Decrement => {
-                        if ui.frame_num_bps.label().parse::<i32>().unwrap() > 2 {
-                            ui.frame_num_bps.set_label(&(label -1).to_string());
-                        }
+                        if ui.frame_num_bps.label().parse::<i32>().unwrap() >= 3 {
+			    if pcbp.process_bps(-1) == Ok(0) {
+                                ui.frame_num_bps.set_label(&(label -1).to_string());
+			    	if pcbp.get_divider_flag() == false {
+				    pcbp.set_divider_flag();
+				}
+			    }
+			}
                     }
-                }
-            }
-            let num_bps_to_update = ui.frame_num_bps.label().parse::<i32>().unwrap() - label;  
-            if pcbp.process_bps(num_bps_to_update) != Ok(0) {
-                panic!("Error process curve breakpoints!");
-            }
-            let root = BitMapBackend::with_buffer(&mut buf, (670/*350 800*/, 480/*300  600*/)).into_drawing_area();     // BMB
-            root.fill(&WHITE)?;
-            root.margin(10, 10, 10, 10);
-            let mut chart = ChartBuilder::on(&root)
-			                      .caption(&pcbp.file_name, ("sans-serif", 40).into_font())
-			                      .x_label_area_size(20)
-			                      .y_label_area_size(60)
-                            .build_cartesian_2d(0f32..x_max, 0f32..y_max)?;
+		    Message::Choice1Msg => {
+		        if pcbp.get_divider_flag() == false {
+			    let choice_v = ui.choice_1.choice().unwrap().parse::<f32>()
+			    if choice_v != Ok(-1.0) {
+				ui.choice_1.set_label(&ui.choice_1.choice().unwrap());
+				if choice_v != Ok(pcbp.section_dvder) {
+				    let tmp: f32 = choice_v.unwrap();
+				    pcbp.set_divider(tmp);
+				}
+			    }
+			    else {
+				    panic!("Error, parse choice selection error!");
+                	    }
+            		}
+		    }
+	      }
+	  }
+        
+          let root = BitMapBackend::with_buffer(&mut buf, (670/*350 800*/, 480/*300  600*/)).into_drawing_area();     // BMB
+          root.fill(&WHITE)?;
+          root.margin(10, 10, 10, 10);
+          let mut chart = ChartBuilder::on(&root)
+			.caption(&pcbp.file_name, ("sans-serif", 40).into_font())
+			.x_label_area_size(20)
+			.y_label_area_size(60)
+                        .build_cartesian_2d(0f32..x_max, 0f32..y_max)?;
             chart.configure_mesh()
-			           .x_labels(5)
-			           .y_labels(5)
-			           .y_label_formatter(&|x| format!("{:.3}", x))
-			           .draw()?;
+			.x_labels(5)
+			.y_labels(5)
+			.y_label_formatter(&|x| format!("{:.3}", x))
+			.draw()?;
 
             let outtv_cloned = pcbp.orig_bps.clone();
             chart.draw_series(LineSeries::new(
@@ -78,7 +105,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 &RED,
             ))?;
 
-    		    chart.draw_series(PointSeries::of_element(
+    	    chart.draw_series(PointSeries::of_element(
                 outtv_cloned,
                 2,
                 &RED,
@@ -98,7 +125,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             app::sleep(0.086);
             app::awake();
 
-		}
+    }
     
     app.run().unwrap();
 
